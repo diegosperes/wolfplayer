504bab110304e9c5bea305f4a3cfc8ee20914708
Diego Péres <diego.peres@corp.globo.com>
2017-07-27 13:53:41 -0300

    Run tests in karma

--- test/player.spec.js
+++ test/player.spec.js
@@ -1,11 +1,17 @@
+import { expect } from 'chai'
+
 import WolfPlayer from '../wolfplayer/index'
 
 describe('Player', () => {
+  afterEach(() => {
+    for (let video of document.querySelectorAll('video')) { video.parentNode.removeChild(video) }
+  })
+
   it('insert video element in DOM with correct attributes', () => {
     let player = new WolfPlayer({ parent: 'body', src: 'teste' })
-    let videoElement = document.querySelector('body > video')
+    let videoElement = document.querySelector('body video')
 
-    expect(videoElement.length).to.be.equal(1)
-    expect(videoElement.src).to.be.equal('teste')
+    expect(videoElement.tagName).to.be.equal('VIDEO')
+    expect(videoElement.src).to.be.equal('http://localhost:9876/teste')
   })
 })
