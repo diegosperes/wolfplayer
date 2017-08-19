c9c5015c79d0b80140a210178dbaad97c8bb46c2
Diego.Péres <speres.diego@gmail.com>
2017-08-19 11:26:52 -0300

    add missing tests of player

---
+++ test/player.spec.js
@@ -0,0 +1,41 @@
+import utils from './utils'
+
+import WolfPlayer from '../src/player'
+import { Plugin } from '../src/base'
+
+class FakePlugin extends Plugin {
+  constructor(manager, options) {
+    super(manager, options)
+    options.constructorSpy(manager, options)
+  }
+}
+
+describe('Player', function() {
+
+  beforeEach(function() {
+    this.options = Object.assign({fakeplugin: {constructorSpy: jasmine.createSpy('spy')}}, utils.baseOptions)
+    this.options.plugins.push(FakePlugin)
+    this.player = new WolfPlayer(this.options)
+  })
+
+  it('should register all plugin events before HOOK_START', function() {
+    expect(this.player.events.LOGGER_ERROR).toBeDefined()
+  })
+
+  it('should use the same manager of player and use correct options', function(done) {
+    this.player.addListener(this.player.events.HOOK_READY, () => {
+      expect(this.options.fakeplugin.constructorSpy).toHaveBeenCalledWith(this.player, this.options.fakeplugin)
+      done()
+    })
+  })  
+
+  it('should create plugin in HOOK_START event', function(done) {
+    let callbackReady = jasmine.createSpy('spy')
+    this.player.addListener(this.player.events.HOOK_READY, callbackReady)
+
+    this.player.addListener(this.player.events.HOOK_READY, () => {
+      expect(this.options.fakeplugin.constructorSpy).toHaveBeenCalledBefore(callbackReady)
+      done()
+    })
+  })
+})
