4073bfbc9cc13f9405ed074b27863a8e795a6d23
Diego Péres <diego.peres@corp.globo.com>
2017-08-25 14:39:05 -0300

    Add modified player in utils to fix tests race conditions

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,5 @@
-import { Logger } from '../src/plugins/index.js'
+import Player from '../src/player'
+import { Logger } from '../src/plugins/index'
 
 const baseOptions = {
   parent: 'body',
@@ -6,6 +7,28 @@ const baseOptions = {
   plugins: [Logger]
 }
 
+const createPlayer = (options=baseOptions) => {
+  /*
+    Override trigger methods from Manager class to check if event is called.
+  */
+  let player = new Player(options)
+  let originalTrigger = player.trigger
+
+  player.waitEvent = function(event, callback) {
+    player._waitingEvent = event
+    player._waitingCallback = callback
+  }
+
+  player.trigger = function(event, args) {
+    originalTrigger.apply(player, [event, args]).then(() => {
+      if (player._waitingEvent === event) player._waitingCallback()
+    })
+  }
+
+  return player
+}
+
 export default {
-  baseOptions
+  baseOptions,
+  createPlayer
 }
